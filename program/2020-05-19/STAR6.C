0c6d438d74a0986b10af90b13908567d144e7eb8
Anil Kumar <ui.anilk@gmail.com>
2020-05-19 07:20:46 +0530

    Stars printing using nested for

---
+++ program/2020-05-19/STAR6.C
@@ -0,0 +1,31 @@
+//    *
+//   ***
+//  *****
+// ********
+//**********
+//============
+// ********
+//  *****
+//   ***
+//    *
+
+//lctr=1, space=0, star=line*2-1
+//lctr++, space++, star-=2
+
+void main() {
+	int line, lctr, space, spctr, star, sctr;
+	clrscr();
+	printf("Enter number of lines: ?\b");
+	scanf("%d", &line);
+
+	for(lctr=1, space=0, star=line*2-1; lctr<=line; lctr++, space++, star-=2) {//line
+		for(spctr=1; spctr<=space; spctr++) {//space
+			printf(" ");
+		}
+		for(sctr=1; sctr<=star; sctr++) {//star
+			printf("*");
+		}
+		printf("\n");
+	}
+	getch();
+}
